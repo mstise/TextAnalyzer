e4884ed327b0993c29bffffcb24d74295be25ce3
Unknown <mthyge11@student.aau.dk>
2017-10-11 09:16:19 +0200

    asd
    Alt mit shit

--- Gensim/CreateDictionary.py
+++ Gensim/CreateDictionary.py
@@ -1,16 +1,8 @@
 from gensim import corpora
 import os
-import xml.etree.ElementTree
 from six import iteritems
 import paths
 
-def what(thisis):
-    return thisis
-
-# collect statistics about all tokens
-whatsthis = list(what(line.lower().split() for line in open('mycorpus.txt')))
-dictionary = corpora.Dictionary(line.lower().split() for line in open('mycorpus.txt'))
-dictionary.add_documents(line.lower().split() for line in open('mycorpus2.txt'))
 # remove stop words and words that appear only once
 #stop_ids = [dictionary.token2id[stopword] for stopword in stoplist
 #            if stopword in dictionary.token2id]
@@ -18,25 +10,26 @@ dictionary.add_documents(line.lower().split() for line in open('mycorpus2.txt'))
 #dictionary.compactify()  # remove gaps in id sequence after words that were removed
 #print(dictionary)
 
-
-body = ""
-header = ""
-bodies = []
 dictionary = corpora.Dictionary()
 
 for filename in os.listdir(paths.get_external_disk_path()):
-    dictionary.add_documents(line.lower().split() for line in open(filename))
+    dictionary.add_documents(line.lower().split() for line in open(paths.get_external_disk_path() + "/" + filename))
 once_ids = [tokenid for tokenid, docfreq in iteritems(dictionary.dfs) if docfreq == 1]
 dictionary.filter_tokens(once_ids)  # remove stop words and words that appear only once
 dictionary.compactify()  # remove gaps in id sequence after words that were removed
-#tokenize
+dictionary.save("newsDict")
+
 class MyCorpus(object):
     def __iter__(self):
-        for line in open('mycorpus.txt'):
-            # assume there's one document per line, tokens separated by whitespace
-            yield dictionary.doc2bow(line.lower().split())
+        for filename in os.listdir(paths.get_external_disk_path()):
+            for string in open(paths.get_external_disk_path() + "/" + filename):
+                # there is one string per document, tokens separated by whitespace
+                yield dictionary.doc2bow(string.lower().split())
 
-texts = [[word for word in document.lower().split()] for document in bodies ]
-
-print(texts)
-
+corpus = MyCorpus()
+counter = 0
+for document in corpus:
+    if counter == 10:
+        break
+    print(document)
+    counter += 1
---
+++ Polyglot-test.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from polyglot.text import Text
+print(u"å")
+#texten = Text("Lars synger duet med Mads Mikkelsen")
