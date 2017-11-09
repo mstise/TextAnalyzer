02a8ba668ba91244e7b1309806785e71589a658e
Michael Stisen <mstise11@student.aau.dk>
2017-11-09 10:02:58 +0100

    New commit

---
+++ LinkedinDisambiguator/Google_scraper.py
@@ -0,0 +1,6 @@
+import google
+
+print (google.filter_result(next(google.search('thomas kastrup larsen'))))
+
+# service = build('customsearch', 'v1', developerKey='AIzaSyDWKM3PN9QdA_LpvJvAJNK3bwiNtKvZ9uo')
+# collection = service.stamps()
