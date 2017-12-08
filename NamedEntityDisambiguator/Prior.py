e21c9c466b1483fa3d579392ae649c17502292c4
Michael Stisen <mstise11@student.aau.dk>
2017-12-08 15:06:57 +0100

    limits amount of priors

--- NamedEntityDisambiguator/Prior.py
+++ NamedEntityDisambiguator/Prior.py
@@ -1,14 +1,10 @@
 import re
 from NamedEntityDisambiguator import Utilities
 
-def find_link(search_term, text):
+def find_link(text):
     if text == None:
         return []
-    search_term = search_term.lower()
-    text = text.lower()
-    with_split = re.findall(r'\[\[[^\]]*\|' + search_term + '\]\]', text)
-    without_split = re.findall(r'\[\[' + search_term + '\]\]', text)
-    return with_split + without_split
+    return re.findall(r'\[\[[^\]]*\]\]', text)
 
 def get_link_names(name):
     if '|' in name:
@@ -32,8 +28,19 @@ def popularityPrior(names, wiki_tree_root):
                 if Utilities.cut_brackets(page_child.tag) == 'revision':
                     for text in page_child:
                         if Utilities.cut_brackets(text.tag) == 'text':
+                            # links = find_link(text.text)
+                            # for link in links:
+                            #     link_names = get_link_names(link)
+                            #     for link_name in link_names:
+                            #         for name in u_names:
+                            #             if link_name[0] == name:
+                            #                 if (len(link_names[1]) < 9 or link_names[1][:9] != 'kategori:') and \
+                            #                         (len(link_names[1]) < 4 or link_names[1][:4] != 'fil:') and \
+                            #                         (len(link_names[1]) < 8 or link_names[1][:8] != 'billede:'):
+                            #                     reference_list.append(link_names)
+
                             for name in u_names:
-                                result = find_link(name, text.text)
+                                result = find_link(text.text)
                                 for link in result:
                                     link_names = get_link_names(link)
                                     if (len(link_names[1]) < 9 or link_names[1][:9] != 'kategori:') and\
@@ -53,7 +60,7 @@ def popularityPrior(names, wiki_tree_root):
                 sub_list_length = len(first_match)
                 matches = [x for x in matches if x not in first_match]
                 sub_list.append([first_match[0][1], sub_list_length/list_length])
-            prior_return_list.append([first_match[0][0], sub_list])
+            prior_return_list.append([first_match[0][0], sub_list.sort(key=lambda x: x[1])[:20]])
         else:
             prior_return_list.append([name, []])
 
