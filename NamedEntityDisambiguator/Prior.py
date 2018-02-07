7eb1af7bc172696e5fad90dd0e184c456ab48553
Michael Stisen <mstise11@student.aau.dk>
2018-02-07 10:52:43 +0100

    more prior testing

--- NamedEntityDisambiguator/Prior.py
+++ NamedEntityDisambiguator/Prior.py
@@ -63,7 +63,7 @@ def popularityPrior(names, wiki_tree_root):
     prior_dict = shelve.open("NamedEntityDisambiguator/dbs/prior_dic")
     for name in names:
         print(name)
-        print(prior_dict[name])
+        print(prior_dict[name.lower()])
 
     prior_return_list = []
     # for name in names:
