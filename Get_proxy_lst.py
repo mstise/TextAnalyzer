81e66deea1989954b1b9a106e96fcbcadbc6ec31
Martin Thygesen <mthyge11@student.aau.dk>
2018-02-09 12:40:38 +0100

    sfd

--- Get_proxy_lst.py
+++ Get_proxy_lst.py
@@ -7,8 +7,8 @@ def get_proxy_lst(proxy_path):
         proxy_lst.append(new_string)
     return proxy_lst
 
-proxy_lst = get_proxy_lst('Proxies')
-print(proxy_lst)
+#proxy_lst = get_proxy_lst('Proxies')
+#print(proxy_lst)
 
 '''import urllib2
 
@@ -17,7 +17,7 @@ proxy = urllib2.ProxyHandler({'https': 'https://mthyge11:an2loper@82.211.50.204:
 auth = urllib2.HTTPBasicAuthHandler()
 opener = urllib2.build_opener(proxy, auth, urllib2.HTTPHandler)
 urllib2.install_opener(opener)
-
+    
 conn = urllib2.urlopen('http://expressvpn.com/what-is-my-ip')
 return_str = conn.read()
 print return_str
