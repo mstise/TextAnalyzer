4ac6a4babfd41e31815eb48e9fbf7c7786910d9c
mstise <mstise11@student.aau.dk>
2018-03-21 10:20:41 +0100

    more testing

--- NamedEntityRecognizer/Retrieve_All_NER.py
+++ NamedEntityRecognizer/Retrieve_All_NER.py
@@ -5,16 +5,16 @@ import re
 import paths
 
 def clean_line(line):
+    str.replace(line, '\'s\"]', '\']')
+    str.replace(line, '[u\"', '[u\'')
+    str.replace(line, '\"]', '\']')
     indices = [m.start() for m in re.finditer('\'', line)]
     grouped_indices =list(zip(indices[0::2], indices[1::2]))
     print(str(grouped_indices))
     print(line)
-    #if grouped_indices != []:
     cleaned_line = line[grouped_indices[0][0]+1 : grouped_indices[0][1]]
     for i, k in grouped_indices[1:len(grouped_indices)]:
         cleaned_line += ' ' + line[i+1 : k]
-    #else:
-    #   cleaned_line = ''
     return cleaned_line
 
 
