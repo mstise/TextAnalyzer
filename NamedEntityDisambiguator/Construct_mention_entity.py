60384aceb83caba68f13df4a21c0a208ad1d390f
mstise <mstise11@student.aau.dk>
2018-03-21 14:25:50 +0100

    more testing

--- NamedEntityDisambiguator/Construct_mention_entity.py
+++ NamedEntityDisambiguator/Construct_mention_entity.py
@@ -48,13 +48,14 @@ def remove_s_modification(priors, prior_dict):
     for i in range(0, len(priors)):
         print('Prior name is: ' + priors[i][0])
         if check_for_ending_s(priors[i][0], priors[i][1]):
-            print('We entered the loop!')
             try_without_s_list.append(priors[i][0])
             try_without_s_list_id.append(i)
     new_priors = popularityPrior(try_without_s_list, prior_dict)
+    print('New priors: ' + new_priors)
     for i in range(0, len(new_priors)):
         new_priors[i][0] = priors[try_without_s_list_id[i]][0]
         priors[try_without_s_list_id[i]] = new_priors[i]
+        print('priors: ' + priors)
 
 def construct_ME_graph(document, recognized_mentions, root, reference_keyphrases, title_of_ent_linking_to_ent, link_anchors_of_ent, ent_ent_coh_dict, prior_dict, alpha=0.45, beta=0.45, gamma=0.1):
     start = time.time()
