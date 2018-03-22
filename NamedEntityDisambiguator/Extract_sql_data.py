2ae771c3c0d353b63247e99f84e8089260c509c7
mstise <mstise11@student.aau.dk>
2018-03-22 11:56:02 +0100

    categories test

---
+++ NamedEntityDisambiguator/Extract_sql_data.py
@@ -0,0 +1,19 @@
+import MySQLdb
+
+def load_sql_file():
+    db = MySQLdb.connect(host="localhost",  # your host, usually localhost
+                         user="duper",  # your username
+                         passwd="an2loper",  # your password
+                         db="wikidb",
+                         charset='utf8')  # name of the data base
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    cur = db.cursor()
+
+    cur.execute("SELECT cl_to" +
+                " FROM categorylinks;")
+
+    for row in cur.fetchall():
+        non_bytestring = row[0].decode("utf-8")
+        print(non_bytestring)
+        #tmp_phrase_lst.append(non_bytestring.replace("_", " ").lower())
