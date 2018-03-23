2927042329154bffe24a1b4ec1efaf971d6a5500
mstise <mstise11@student.aau.dk>
2018-03-23 10:22:32 +0100

    more testing

--- NamedEntityDisambiguator/Extract_sql_data.py
+++ NamedEntityDisambiguator/Extract_sql_data.py
@@ -21,10 +21,10 @@ def load_sql_file():
             entities = []
             for entity in [s.strip() for s in entity_row.splitlines()]:
                 entities.append(entity)
-                if entity == 'TRIGE':
-                    print(str(category))
             for entity in entities:
                 append_val(phrase_dict, key=entity, val=category)
+                if entity == 'TRIGE':
+                    print(str(category))
                 #print(entity.lower() + " : ")# + str(phrase_dict[entity.lower()]))
         #tmp_phrase_lst.append(non_bytestring.replace("_", " ").lower())
     #print('TRIGE: ' + str(phrase_dict['trige']))
