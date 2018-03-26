1433c16e4ba295c8d6592a2c3e3d0db823af6a35
mstise <mstise11@student.aau.dk>
2018-03-26 14:08:31 +0200

    testing once again

--- NamedEntityDisambiguator/Construct_mention_entity.py
+++ NamedEntityDisambiguator/Construct_mention_entity.py
@@ -119,7 +119,7 @@ def construct_ME_graph(document, recognized_mentions, root, reference_keyphrases
     entities_for_sim_score = []
     for ent in entities:
         entities_for_sim_score.append(ent)
-    removed_priors = remove_large_priors(priors, entities_for_sim_score, candidates_dic)
+    #removed_priors = remove_large_priors(priors, entities_for_sim_score, candidates_dic)
 
     #reference_keyphrases = shelve.open(reference_keyphrases)
     #title_of_ent_linking_to_ent = shelve.open(title_of_ent_linking_to_ent)
@@ -128,7 +128,7 @@ def construct_ME_graph(document, recognized_mentions, root, reference_keyphrases
 
     simscore_dic = keyphrase_similarity(entities_for_sim_score, candidates_dic, [word for line in open(document, 'r') for word in util.split_and_delete_special_characters(line)], reference_keyphrases, title_of_ent_linking_to_ent, link_anchors_of_ent, category_kps)
 
-    populate_sim_score(removed_priors, simscore_dic)
+    #populate_sim_score(removed_priors, simscore_dic)
 
     #reference_keyphrases.close()
     #title_of_ent_linking_to_ent.close()
