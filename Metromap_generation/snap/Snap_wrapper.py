5332aaf0e925cafa2331808adacdbad147085c47
Martin Thygesen <mthyge11@student.aau.dk>
2018-04-11 10:36:35 +0200

    fji

--- Metromap_generation/snap/Snap_wrapper.py
+++ Metromap_generation/snap/Snap_wrapper.py
@@ -3,19 +3,18 @@ from scipy.sparse import csr_matrix
 from Metromap_generation.MatrixUtils import init_matrix
 
 def snap(num_docs):
-    print('MAX CLUSTERS: ' + str(int(num_docs/2)))
     print('NUM DOCS: ' + str(num_docs))
     os.system("./snap/bigclam -o:snap/snapout/ -c:-1 -i:snap/edges -l:snap/labels -nc:" + str(num_docs) + " -mc:1 -xc:" + str(num_docs))
 
-def get_data(term2idx, V):
+def get_data(term2idx, resetter, V):
     file = open('snap/snapout/cmtyvv.txt', 'r')
     lines = file.readlines()
     num_cmtys = len(lines)
-    W = init_matrix((list(V.shape)[0], num_cmtys), 0.0)
+    W = init_matrix((list(V.shape)[0], num_cmtys), resetter)
     cmty_counter = 0
     for line in lines:
         tuples = line[:-1].split('\t')
-        for tuple in tuples:
+        for tuple in tuples[:-1]:
             term, weight = tuple.split(',')
             W[term2idx[term], cmty_counter] = weight
         cmty_counter += 1
